e7c5bbb5355df70ab98660fee1b6c07e72f37a69
高尚先生 <58834725+shuheng-2834@users.noreply.github.com>
2022-07-12 20:56:22 +0800

    1252. 奇数值单元格的数目

---
+++ src/Array/Pro/No1252/Main.java
@@ -0,0 +1,53 @@
+package Array.Pro.No1252;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * @author 钟书恒
+ * @ClassName Main
+ * @date 2022/7/12 15:41
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        int[][] indices = new int[][]{{17, 16}, {26, 31}, {19, 12}, {22, 24}, {17, 28}, {23, 21}, {27, 32}, {23, 27}, {23, 33}, {18, 7}, {4, 20}, {0, 31}, {25, 33}, {5, 22}};
+        new Solution().oddCells(28, 38, indices);
+    }
+}
+
+class Solution {
+    public int oddCells(int m, int n, int[][] indices) {
+        int count = 0;
+        int[][] sq = new int[m][n];
+        for (int[] ints : sq) {
+            Arrays.fill(ints, 0);
+        }
+        for (int[] index : indices) {
+            int row = index[0];
+            int col = index[1];
+
+            // 矩阵的行加一
+            for (int i1 = 0; i1 < sq[row].length; i1++) {
+                sq[row][i1] += 1;
+            }
+
+            // 矩阵的列加一
+            for (int i1 = 0; i1 < sq.length; i1++) {
+                sq[i1][col] += 1;
+            }
+        }
+
+        int all = 0;
+        for (int[] ints : sq) {
+            for (int anInt : ints) {
+                if (anInt % 2 == 1 || anInt == 1) {
+                    count += 1;
+                } else if (anInt % 2 == 0) {
+                    all += 1;
+                }
+            }
+        }
+        return count;
+    }
+}

714a3cb7f54c48eccc9442932e9d9de34d3eee3d
高尚先生 <58834725+shuheng-2834@users.noreply.github.com>
2022-07-13 16:26:44 +0800

    14. 最长公共前缀(优化+2)

--- src/Array/Pro/No14/Main.java
+++ src/Array/Pro/No14/Main.java
@@ -15,16 +15,11 @@ public class Main {
 
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) return "";
-        // 当前公共前缀
-        int minIndex = 0;
-        for (int i = 1; i < strs.length; i++) {
-            if (strs[i].length() <= strs[i - 1].length()) {
-                minIndex = i;
-            }
+        if (strs.length == 0) {
+            return "";
         }
-        System.out.println(strs[minIndex]);
-        String flag = strs[minIndex];
+
+        String flag = strs[0];
         for (String str : strs) {
             while (!str.startsWith(flag)) {
                 if (flag.length() == 0) return "";
